e9477c6f4f99febd2d1ceb9942e9d6c978eb7ba0
swagatika1224 <81203604+swagatika1224@users.noreply.github.com>
2021-08-25 19:05:30 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,40 @@
-function startClassification() {
-    navigator.mediaDevices.getUserMedia({ audio: true});
-    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/FYtYKUlOH/model.json', modelReady);
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true});
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/u02fOYRBx/model.json', modelReady);
+}
+
+function modelReady(){
+  classifier.classify(gotResults);
+}
+var dog = 0;
+var cat = 0;
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    random_number_r = Math.floor(Math.random() * 255) + 1;
+    random_number_g = Math.floor(Math.random() * 255) + 1;
+    random_number_b = Math.floor(Math.random() * 255) + 1;
+
+
+    document.getElementById("result_label").innerHTML = 'Detected voice is of  - '+ results[0].label;
+    document.getElementById("result_count").innerHTML = 'Detected Dog - '+dog+ ' Detected Cat - '+cat;
+    document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+    document.getElementById("result_count").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+
+    img = document.getElementById('animal_image');
+
+    if (results[0].label == "Barking") {
+      img.src = 'https://static.thenounproject.com/png/751-200.png';
+      dog = dog+1;
+    } else if (results[0].label == "Meowing") {
+      img.src = 'https://previews.123rf.com/images/antoninabiz/antoninabiz1910/antoninabiz191000001/133043113-cat-icon-meowing-cat-gray-cat-with-a-whiskers-in-a-round-icon-modern-flat-vector-illustration-on-whi.jpg';
+      cat = cat + 1;
+    } else{
+      img.src = 'https://media.istockphoto.com/photos/watercolor-textured-background-picture-id887755698?k=6&m=887755698&s=612x612&w=0&h=_yEUF8gLpWjZv5IgwuWkecNVt4X4P7vpuFBKCWIuR44=';
     }
-    
-    function modelReady() {
-    classifier.classify(gotResults);
-    }
-    
-    function gotResults(error, results) {
-    console.log(gotResults);
-    }
+  }
+}
